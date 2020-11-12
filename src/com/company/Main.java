8e29f783b9271e9093d46f6233dd706dd079070e
Ajit <ajitkrsingh0786@gmail.com>
2020-11-12 07:53:05 +0530

    Top economical bowlers in 2015

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import implement.EconomicalBowlersOf2015;
 import implement.ExtraRunsConceded;
 import implement.MatchesPlayedPerYear;
 import implement.MatchesWonByEachTeam;
@@ -13,7 +14,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter 1 to see the total matches played per year\n" +
                 "Enter 2 to see the total number of matches won by each\n" +
-                "Enter 3 to see the total extra runs conceded by each team in 2016");
+                "Enter 3 to see the total extra runs conceded by each team in 2016\n" +
+                "Enter 4 to see the top 10 economical bowlers in 2015");
 
         int i = sc.nextInt();
          switch (i) {
@@ -28,6 +30,10 @@ public class Main {
              case 3:
                  ExtraRunsConceded extra_runs = new ExtraRunsConceded();
                  extra_runs.extraRunConcededByEachTeam();
+                 break;
+             case 4:
+                 EconomicalBowlersOf2015 economical_bowlers = new EconomicalBowlersOf2015();
+                 economical_bowlers.allEconomicalBowlersOf2015();
                  break;
              default:
                  System.out.println("Question number is wrong\n Enter correct the number");
